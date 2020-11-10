990d92afe0f8141107216608d52472a711ede2ca
Adria Gascon <adriagascon@gmail.com>
2020-11-10 15:17:53 +0100

    Update setup.py to point to the main repo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='shuffleddp',
     version='1.0.0',
-    url='https://github.com/adriagascon/amplification-by-shuffling.git',
+    url='https://github.com/BorjaBalle/amplification-by-shuffling.git',
     author='Borja Balle',
     author_email='bballe@gmail.com',
     description='Python implementation of the privacy amplification by shuffling results proved in the privacy blanket paper',
